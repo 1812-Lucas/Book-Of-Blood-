a4dd41c2ac6c05b25dbc7295be2a911a7701094d
Lucas-1812 <lucasetorres2013@gmail.com>
2023-05-09 19:28:35 -0300

    quitando los cambios inutiles (no funciono lo del combate)

--- Assets/Scripts/CombatPosition.cs
+++ Assets/Scripts/CombatPosition.cs
@@ -6,13 +6,13 @@ using Cinemachine;
 public class CombatPosition : MonoBehaviour
 {
     //public EnemyAldeano enemyAldean;
-    
+
     public Enemy enemyy;
     public Combat combatscript;
     public List<GameObject> enemyGObj;
     public Transform enemytransf;
     public GameObject areaWhereTheEnemySpawns;
-    public GameObject areWhereTheEnemySpawns2;
+
     public bool battlePosition = false;
     public Rigidbody playerRB;
     public Camera mainCamera;
@@ -29,9 +29,7 @@ public class CombatPosition : MonoBehaviour
     public VigorCardsDisplay ScriptVigorCardDisplaySlot6;
     public StadisticPlayer stadisticPlayerScript;
 
-    public GameObject magicSeal1;
 
-    public GameObject magicSeal2;
 
     public float ContadorTransicion;
 
@@ -39,10 +37,10 @@ public class CombatPosition : MonoBehaviour
     {
         myGM = GameManager.instance;
     }
-    
+
     public void salircombate()
     {
-       
+
         enemiesreminder--;
         if (enemiesreminder <= 0)
         {
@@ -55,18 +53,18 @@ public class CombatPosition : MonoBehaviour
             player.enabled = true;
             playerRB.constraints = RigidbodyConstraints.None;
             playerRB.constraints = RigidbodyConstraints.FreezeRotation;
-            
+
             Debug.Log("Saliste del combate");
             //playerRB.freezeRotation = false;
             //player.enabled = true;
-            
+
         }
     }
 
     public void combatON()
     {
-        if(magicSeal1 == true)
-        {
+
+
         Cursor.lockState = CursorLockMode.Confined;
         SwitchCamera(cameras[1]);
         battlePosition = true;
@@ -82,60 +80,41 @@ public class CombatPosition : MonoBehaviour
         deckscript.DrawCards();
         vigordeckscript.DrawCards();
 
-        }
 
-        
-        if(magicSeal2 == true)
-        {
-            Cursor.lockState = CursorLockMode.Confined;
-            SwitchCamera(cameras[2]);
-            battlePosition = true;
-            camerascript.enabled = false;
-            myGM.activeUI();
-            player.enabled = false;
-            Vector3 direccion = new Vector3(7, 0, 12);
-            transform.LookAt(direccion);
-            mainCamera.transform.LookAt(enemytransf);
-            playerRB.constraints = RigidbodyConstraints.FreezeAll;
-            //playerRB.freezeRotation = true;
-            Debug.Log("Entraste en combate");
-            deckscript.DrawCards();
-            vigordeckscript.DrawCards();
 
-        }
+
 
         //stadisticPlayerScript.vigor = 1;
-        
+
     }
     public void SwitchCamera(CinemachineVirtualCamera camera)
     {
-       
+
         camera.Priority = 10;
         ActiveCamera = camera;
 
 
-       // camerascript.canMoveCamera = false;
+        // camerascript.canMoveCamera = false;
 
         foreach (CinemachineVirtualCamera c in cameras)
         {
             if (c != camera && c.Priority != 0)
             {
                 c.Priority = 0;
-                
+
 
             }
         }
 
-      
+
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.layer == 9)
         {
-            if (magicSeal1 == true)
-            {
-                
+
+
             Destroy(other.gameObject);
             camerascript.canMoveCamera = false;
             enemiesreminder = 1;
@@ -147,38 +126,22 @@ public class CombatPosition : MonoBehaviour
             ScriptVigorCardDisplaySlot5.setenemy(actualenemy);
             ScriptVigorCardDisplaySlot6.setenemy(actualenemy);
             Destroy(areaWhereTheEnemySpawns.gameObject);
-            StartCoroutine(CamaraTransicionCombate());
+            //StartCoroutine(CamaraTransicionCombate());
             combatON();
 
-            }
-
-            if (magicSeal2 == true)
-            {
-                Destroy(other.gameObject);
-                camerascript.canMoveCamera = false;
-                enemiesreminder = 1;
-                Enemy actualenemy = Instantiate(enemyGObj[Random.Range(0, enemyGObj.Count)], areWhereTheEnemySpawns2.transform.position, areWhereTheEnemySpawns2.transform.rotation).GetComponent<Enemy>();
-                actualenemy.Setcombat(this);
-                actualenemy.SetPlayer(stadisticPlayerScript);
-                combatscript.setenemy(actualenemy);
-                ScriptVigorCardDisplaySlot4.setenemy(actualenemy);
-                ScriptVigorCardDisplaySlot5.setenemy(actualenemy);
-                ScriptVigorCardDisplaySlot6.setenemy(actualenemy);
-                Destroy(areaWhereTheEnemySpawns.gameObject);
-                StartCoroutine(CamaraTransicionCombate());
-                combatON();
-            }
-
         }
+
+
+
     }
+}  
+    //IEnumerator CamaraTransicionCombate()
+    //{
+    //    yield return new WaitForSeconds(ContadorTransicion);
 
-    IEnumerator CamaraTransicionCombate()
-    {
-        yield return new WaitForSeconds(ContadorTransicion);
 
+    //    camerascript.canMoveCamera = false;
 
-        camerascript.canMoveCamera = false;
+    //    yield return null;
+    //}
 
-        yield return null;
-    }
-}
