cb43f3888c57166329387cd5cf7c129869ead0bc
Lucas-1812 <lucasetorres2013@gmail.com>
2023-05-16 18:20:52 -0300

    fondo texto, best fit, colores letras

--- Assets/Scripts/ConsoleDisplay.cs
+++ Assets/Scripts/ConsoleDisplay.cs
@@ -7,20 +7,27 @@ public class ConsoleDisplay : MonoBehaviour
 {
     public Text consoleText;
     public float messageTime = 1000f;
-
+    
     void Start()
     {
         consoleText.text = "";
+        
     }
 
+    public void Update()
+    {
+        
+    }
     void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
+        
     }
 
     void OnDisable()
     {
         Application.logMessageReceived -= HandleLog;
+        
     }
 
     void HandleLog(string logString, string stackTrace, LogType type)
