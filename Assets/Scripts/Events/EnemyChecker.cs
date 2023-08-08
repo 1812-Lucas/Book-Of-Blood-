2f3146aa70fe6bd1e56eaf944d024755bc63e60b
Lucas-1812 <lucasetorres2013@gmail.com>
2023-08-08 13:48:56 -0300

    posicionada la reja, hecha la animacion y posicionada la camara nueva para el evento

--- Assets/Scripts/Events/EnemyChecker.cs
+++ Assets/Scripts/Events/EnemyChecker.cs
@@ -9,11 +9,15 @@ public class EnemyChecker : MonoBehaviour
     public int numEnemies;
     [SerializeField] UnityEvent Ev_Activa;
 
+    public Animator rejaNivel1;
+
     private void Update()
     {
         numEnemies = GameObject.FindGameObjectsWithTag("EnemyOpen").Length;
         if (numEnemies <= 0)
         {
+            rejaNivel1.SetBool("RejaBoolNivel1", true);
+            rejaNivel1.CrossFade("RejaAnimationNivel1", 0f);
             EnemyKilled();
         }
     }
