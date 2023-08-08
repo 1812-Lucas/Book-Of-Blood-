26250e21acf04f26646263cd474d76a6fb2c65e0
Marco <marco.lavacchielli@davinci.edu.ar>
2023-08-08 16:07:32 -0300

    Revert "nuevo cambio"
    This reverts commit b28476358b3301ae3a6427c265e2c871a2597f8e.

--- Assets/Scripts/Events/EnemyChecker.cs
+++ Assets/Scripts/Events/EnemyChecker.cs
@@ -8,10 +8,8 @@ using Cinemachine;
 public class EnemyChecker : MonoBehaviour
 {
     public int numEnemies;
-    public int enemy2Contador;
-    public int enemy2ContadorCamera3;
+    public float enemy2Contador;
     [SerializeField] UnityEvent Ev_Activa;
-    [SerializeField] UnityEvent Ev_Exit;
     public CinemachineVirtualCamera ActiveCamera;
     public List<CinemachineVirtualCamera> CamarasCombat2;
 
@@ -26,20 +24,19 @@ public class EnemyChecker : MonoBehaviour
             rejaNivel1.SetBool("RejaBoolNivel1", true);
             rejaNivel1.CrossFade("RejaAnimationNivel1", 0f);
             StartCoroutine(CameraCombatTransition());
-            StartCoroutine(CameraCombatTransition2());
             EnemyKilled();
         }
     }
     public void SwitchCameraCombat(CinemachineVirtualCamera Combat2)
     {
-        Combat2.Priority = 11;
+        Combat2.Priority = 10;
         ActiveCamera = Combat2;
 
         foreach (CinemachineVirtualCamera c in CamarasCombat2)
         {
-            if (c != Combat2 && c.Priority != 11)
+            if (c != Combat2 && c.Priority != 0)
             {
-                c.Priority = 11;
+                c.Priority = 0;
             }
         }
 
@@ -49,17 +46,8 @@ public class EnemyChecker : MonoBehaviour
     {
         yield return new WaitForSeconds(enemy2Contador);
         SwitchCameraCombat(CamarasCombat2[1]);
-        //SwitchCameraCombat(CamarasCombat2[2]);
-       // Ev_Exit.Invoke();
-
-        yield return null;
-    }
-    IEnumerator CameraCombatTransition2()
-    {
-        yield return new WaitForSeconds(enemy2ContadorCamera3);
-        
-        SwitchCameraCombat(CamarasCombat2[2]);
-        Ev_Exit.Invoke();
+        //transition();
+        //Invoke("Playerenabled", 2.5f);
 
         yield return null;
     }
