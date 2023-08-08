0cba5c381ab077190cc1920e48e8d3382e66308a
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-08-07 21:52:52 -0300

    Agregue un sonido que hace "wuooopm"
    es para cuando prendes las luces

--- Assets/Scripts/Events/LightEvent.cs
+++ Assets/Scripts/Events/LightEvent.cs
@@ -6,6 +6,16 @@ using UnityEngine.Events;
 public class LightEvent : MonoBehaviour
 {
 
+    public AudioSource MyAudioSource;
+    public AudioClip LightOnAudio;
+    //public AudioClip[] audiosLightAr;
+
+    public void PlayAudio(AudioClip AC)
+    {
+        MyAudioSource.clip = AC;
+        MyAudioSource.Play();
+    }
+
     [SerializeField] UnityEvent Ev_Activa;
 
     private void OnTriggerEnter(Collider other)
@@ -14,6 +24,7 @@ public class LightEvent : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             Ev_Activa.Invoke();
+            PlayAudio(LightOnAudio);
         }
 
     }
