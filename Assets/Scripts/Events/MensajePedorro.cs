83e468cbf3f846b0052f35b81348b551da1bc2a8
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-08-08 21:28:32 -0300

    Sonidos de reja
    Ya hay sonidos de reja en el primer y segundo nivel

---
+++ Assets/Scripts/Events/MensajePedorro.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MensajePedorro : MonoBehaviour
+{
+    public void start()
+    {
+        Debug.Log("<color=Green><i> A gate was open </i></color>");
+    }
+}
